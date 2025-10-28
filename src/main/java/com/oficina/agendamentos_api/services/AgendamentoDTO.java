111bf911845955d0dc570056629d6de8389d7f47
NikRedFox <nikolasbs11@gmail.com>
2025-10-28 16:31:51 -0300

    Finished API

--- src/main/java/com/oficina/agendamentos_api/services/AgendamentoDTO.java
+++ src/main/java/com/oficina/agendamentos_api/services/AgendamentoDTO.java
@@ -1,9 +1,11 @@
 package com.oficina.agendamentos_api.services;
 
+import java.time.LocalDateTime;
+
 public class AgendamentoDTO {
     private Long clienteId;
     private Long servicoId;
-    private String dataAgendada;
+    private LocalDateTime dataAgendada;
     private String observacoes;
     private String status; // você pode usar String aqui e depois converter para o enum
 
@@ -14,8 +16,8 @@ public class AgendamentoDTO {
     public Long getServicoId() { return servicoId; }
     public void setServicoId(Long servicoId) { this.servicoId = servicoId; }
 
-    public String getDataAgendada() { return dataAgendada; }
-    public void setDataAgendada(String dataAgendada) { this.dataAgendada = dataAgendada; }
+    public LocalDateTime getDataAgendada() { return dataAgendada; }
+    public void setDataAgendada(LocalDateTime dataAgendada) { this.dataAgendada = dataAgendada; }
 
     public String getObservacoes() { return observacoes; }
     public void setObservacoes(String observacoes) { this.observacoes = observacoes; }
